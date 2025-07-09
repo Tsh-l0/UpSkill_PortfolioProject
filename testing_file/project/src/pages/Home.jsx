3faeb59ec4e3862faee9f3def6054887da1f8def
Tsholofelo <kodingwithTsh0l0@gmail.com>
2025-07-09 09:05:04 +0200

    Login Page

--- testing_file/project/src/pages/Home.jsx
+++ testing_file/project/src/pages/Home.jsx
@@ -24,14 +24,7 @@ const Home = () => {
 	return (
 		<div className="homepage">
 		{/* Navbar */}
-		<nav className="navbar">
-			<ul className="nav-links">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Blog</a></li>
-				<li><a href="#">Profile</a></li>
-				<li><a href="#">Dashboard</a></li>
-			</ul>
-		</nav>
+		
 
 		{/* Hero Section */}
 		<section
