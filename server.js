ae5fe1eff4a88743c5d1e40f6dc05a68e4ac68e9
The-deliverator <mdavoo@gmail.com>
2025-07-09 11:55:00 -0700

    changed back to port 5000

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/api/users", userRoutes);
 app.use(errorHandler);
 
 // 🖥️ Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
